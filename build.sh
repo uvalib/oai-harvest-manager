7ec29c08664abab61242d6e9de5bfa9f92ca985c
Robert Haschart <rh9ec@virginia.edu>
2023-08-10 16:07:21 +0000

    Omit git tag from name of target jar

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.el7_9.x86_64
 
+PATH=/usr/local/bin:$PATH
+
 mvn -s settings.xml -Dmaven.test.skip=true  dependency:copy-dependencies compile package
 
