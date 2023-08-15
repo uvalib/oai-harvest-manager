e6a468c0476c6a89cce60aaba4c9ce52f6320a60
Robert Haschart <rh9ec@virginia.edu>
2023-08-15 15:58:09 +0000

    remove echo from script

--- harvestAll.sh
+++ harvestAll.sh
@@ -2,7 +2,7 @@
 
 HOMEDIR=$( (cd -P $(dirname $0) && pwd) )
 
-CURRENTDIR=$( (cd -P $(dirname $*) && pwd) )
+CONFIGDIR=$( (cd -P $(dirname $*) && pwd) )
 CONFIG=$(basename $*)
 
 CLASSPATH="$HOMEDIR/target/oai-harvest-manager-1.2.1.jar:$HOMEDIR/target/lib/*"
@@ -12,6 +12,6 @@ JAVAARGS=""
 MAINCLASS="nl.mpi.oai.harvester.control.Main"
 
 #java -cp ../oai-harvest-manager/target/oai-harvest-manager-1.2.1.8ff86a.jar:../oai-harvest-manager/target/lib nl.mpi.oai.harvester.control.Main  ./config.xml
-cd $CURRENTDIR
-echo java -classpath $CLASSPATH $JAVAARGS $MAINCLASS $CURRENTDIR/$CONFIG 
-java -classpath $CLASSPATH $JAVAARGS $MAINCLASS $CURRENTDIR/$CONFIG 
+cd $CONFIGDIR
+#echo java -classpath $CLASSPATH $JAVAARGS $MAINCLASS $CONFIGDIR/$CONFIG 
+java -classpath $CLASSPATH $JAVAARGS $MAINCLASS $CONFIGDIR/$CONFIG 
